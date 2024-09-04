82fb9fc398fc10f2b0bc0ef48e014ca0ef9ad50e
Ammad-Ashraf <ashrafammad1@gmail.com>
2024-09-04 20:38:44 +0500

    Set up routing in Express.js

--- app.js
+++ app.js
@@ -2,12 +2,40 @@ const express=require('express');
 const res = require('express/lib/response');
 const app=express();
 
-app.get('/',(req,res)=>
-{
-  res.send('Hello World!');
+// GET route for the homepage
+app.get('/', (req, res) => {
+  res.send('Welcome to the Online Teaching Platform');
 });
+
+// PUT route for /user
+app.put('/user/update', (req, res) => {
+  res.send('User information update');
+});
+
+// DELETE route for /user
+app.delete('/user', (req, res) => {
+  res.send('DELETE request to /user');
+});
+
+// GET route for the About page
+app.get('/about', (req, res) => {
+  res.send('About Us page');
+});
+
+// GET route for the Contact page
+app.get('/contact', (req, res) => {
+  res.send('Contact Us page');
+});
+
+app.post('/user/store',(req,res)=>
+{
+  res.send('User information stored');
+});
+
 
 app.listen(3000,()=>
 {
   console.log('Server is running on http://localhost:3000');
 });
+
+
